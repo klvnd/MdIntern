5b2e65aa523c934f6d29042fba59b0f275f3b784
Kelvin Dwipangga <144301420+klvnd@users.noreply.github.com>
2024-08-11 04:08:19 +0700

    update: add logic Interface FirstScreen

--- app/src/main/java/com/klvnd/mdintern/MainActivity.kt
+++ app/src/main/java/com/klvnd/mdintern/MainActivity.kt
@@ -1,20 +1,43 @@
 package com.klvnd.mdintern
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.klvnd.mdintern.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+
+        binding.btnCheck.setOnClickListener {
+            val inputText = binding.etPalindrome.text.toString()
+            val result = if (palindromeCheck(inputText)) "isPalindrome" else "notPalindrome"
+
+            AlertDialog.Builder(this)
+                .setTitle("Palindrome Check")
+                .setMessage(result)
+                .setPositiveButton("OK") { dialog, _ ->
+                    dialog.dismiss()
+                }
+                .show()
         }
+
+        binding.btnNext.setOnClickListener {
+            Toast.makeText(this, "Go to Second Screen", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun palindromeCheck(text: String): Boolean {
+        val sanitizedText = text
+            .replace("\\s".toRegex(), "")
+            .lowercase()
+        return sanitizedText == sanitizedText.reversed()
     }
 }
